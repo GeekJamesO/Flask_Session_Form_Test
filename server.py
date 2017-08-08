77eb21dfa121aa793a35e80e8fc2ffda603347f1
JamesORourke <geekjamesorourkejr@icloud.com>
2017-08-08 11:40:56 -0700

    added session to example

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key="ThisIsSecret"
+@app.route('/')
+def index():
+    return render_template("index.html")
+@app.route('/users', methods=['POST'])
+def create_user():
+    print "Got Post Info"
+    # we'll talk about the following two lines after we learn a little more
+    # about forms
+    print request.form
+    session['name'] = request.form['name']
+    session['email'] = request.form['email']
+    # redirects back to the '/' route
+    return redirect('/show')
+@app.route('/show')
+def show_user():
+    return render_template('user.html', name=session['name'], email=session['email'])
+
+app.run(debug=True) # run our server
